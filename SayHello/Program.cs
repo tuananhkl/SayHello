4e6c774c5605af3c08371405a47e44f6f3af195a
Nguyen Tuan Anh <tuananhkl99@gmail.com>
2023-12-07 13:00:52 +0700

    First Commit

---
+++ SayHello/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+
+Console.Write("Welcome! Please enter your name: ");
+var name = Console.ReadLine();
+
+Console.WriteLine($"Hello and Welcome {name} !");
